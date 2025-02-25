4eb0779a71800149d5eac01b325aed22366b417e
George Jimy <knivespeans@gmail.com>
2025-02-25 17:26:38 -0500

    Paracaidas

--- Assets/Scripts/Paracaidismo/ParachuteFall.cs
+++ Assets/Scripts/Paracaidismo/ParachuteFall.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 
 public class ParachuteFall : MonoBehaviour
 {
+    public GameObject parachute;
 
     private float earthGravity = -9.81f;
     private float moonGravity = -0.5f;
     private bool parachuteBool = false;
-
+    private bool impulseEnable = true;
+    private float timer;
     private Vector3 velocity;
 
     private void Start()
@@ -21,22 +23,44 @@ public class ParachuteFall : MonoBehaviour
 
     private void Update()
     {
-        
-        
+        var localAvatar = SpatialBridge.actorService.localActor.avatar;
+
+        parachute.transform.position = localAvatar.position;
+        parachute.transform.rotation = localAvatar.rotation;
+
+        parachute.SetActive(parachuteBool);
+
+        if (!impulseEnable)
+        {
+            timer += Time.deltaTime;
+            if (timer > 3f)
+            {
+                impulseEnable = true;
+                timer = 0f;
+            }
+        }
+
+        velocity.y = Mathf.Abs(SpatialBridge.actorService.localActor.avatar.velocity.y) + 0.5f;
+
+
     }
 
     public void OpenParachute()
     {
         parachuteBool = true;
         Physics.gravity = new Vector3(0, moonGravity, 0);
-        var localAvatar = SpatialBridge.actorService.localActor.avatar;
+        if (impulseEnable)
+        {
+            SpatialBridge.actorService.localActor.avatar.AddForce(new Vector3(0f, velocity.y));
+            impulseEnable = false;
+        }
+        
     }
 
     public void CloseParachute()
     {
         parachuteBool = false;
         Physics.gravity = new Vector3(0, earthGravity, 0);
-        var localAvatar = SpatialBridge.actorService.localActor.avatar;
     }
 
 
